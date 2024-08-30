c0e8d68f70c95b084fb8462f8381c8da5f7f6998
Thimmaiah KK <149201963+ThimmaiahKK71@users.noreply.github.com>
2024-08-30 20:27:00 +0530

    Update numbergamecodsoftA70.java

--- numbergamecodsoftA70.java
+++ numbergamecodsoftA70.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class numbergamecodsoftA70 {
-
-    // Function that implements the number guessing game
     public static void guessingNumberGame() {
         Scanner sc = new Scanner(System.in);
         int rounds = 0;
@@ -11,23 +9,18 @@ public class numbergamecodsoftA70 {
         while (true) {
             // Increment round counter
             rounds++;
-
-            // Generate a random number between 1 and 100
             int number = 1 + (int) (100 * Math.random());
 
-            // Given K trials
             int K = 5;
             int i, guess;
             int score = 0;
 
             System.out.println("Round " + rounds + ": A number is chosen between 1 to 100. Guess the number within " + K + " trials.");
 
-            // Iterate over K Trials
             for (i = 0; i < K; i++) {
                 System.out.println("Attempt " + (i + 1) + ": Guess the number:");
                 guess = sc.nextInt();
 
-                // If the number is guessed
                 if (number == guess) {
                     System.out.println("Congratulations! You guessed the number.");
                     score = K - i; // Score is based on remaining attempts
@@ -48,7 +41,6 @@ public class numbergamecodsoftA70 {
             System.out.println("Your score in this round: " + score);
             System.out.println("Total score: " + totalScore);
 
-            // Ask the user if they want to play another round
             System.out.println("Do you want to play again? (yes/no)");
             String playAgain = sc.next();
 
@@ -60,7 +52,6 @@ public class numbergamecodsoftA70 {
         System.out.println("Game over! Your final score after " + rounds + " rounds is: " + totalScore);
     }
 
-    // Driver Code
     public static void main(String arg[]) {
         // Function Call
         guessingNumberGame();
